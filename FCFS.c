bc441f6b00e220a84a9888ac7ad8f92e76fc3bc8
AnishaSingh5298 <62662304+AnishaSingh5298@users.noreply.github.com>
2021-06-29 00:17:40 +0530

    Add files via upload

---
+++ FCFS.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+void waitingtime(char p[],int n,int bt[], int wt[] )
+{
+	int i;
+	wt[0]=0;
+	for(i=1;i<n;i++)
+	wt[i]=bt[i-1]+wt[i-1];
+}
+void turnaroundtime(char p[],int n,int bt[], int wt[], int tat[] )
+{
+	int i;
+	for(i=0;i<n;i++)
+	tat[i]=bt[i]+wt[i];
+	
+}
+void avgtime(char p[],int n,int bt[])
+{
+	int i, wt[n],tat[n],totalwt=0,totaltat=0;
+	waitingtime(p,n,bt,wt);
+	turnaroundtime(p,n,bt,wt,tat);
+	printf("Process    Burst Time    Waiting Time    Turn Around Time\n");
+	for(i=0;i<n;i++)
+	{
+		totalwt=totalwt + wt[i];
+		totaltat=totaltat + tat[i];
+		printf("%d",(i+1));
+		printf("               %d",bt[i]);
+		printf("               %d",wt[i]);
+		printf("               %d\n",tat[i]);
+		
+	}
+	float x=(float)totalwt/n;
+	float y=(float)totaltat/n;
+	printf("Average waiting time = %f\n",x);
+	printf("Average turn around time = %f\n",y);
+	
+}
+
+int main()
+{
+	int i,n,bt[100];
+	char p[100];
+	printf("Enter no of processes: ");
+	scanf("%d",&n);
+	printf("Enter the names of processes\n");
+	for(i=0;i<n;i++)
+	{
+	scanf("%s",&p[i]);
+    }
+	printf("Enter the CPU burst time according to the processes\n");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&bt[i]);
+    }
+	avgtime(p,n,bt);
+	return 0;
+	
+	
+}
